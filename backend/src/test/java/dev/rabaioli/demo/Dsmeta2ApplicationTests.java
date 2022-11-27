fcb470da864d4ee947b19fa1467f39db10ae4415
devrabaioli <devrabaioli@gmail.com>
2022-11-27 12:58:53 +0000

    firstcommit

---
+++ backend/src/test/java/dev/rabaioli/demo/Dsmeta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rabaioli.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmeta2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
