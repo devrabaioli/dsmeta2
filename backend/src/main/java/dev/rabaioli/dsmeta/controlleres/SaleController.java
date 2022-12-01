30cf142aa5d1487d0c2bcc26d4b6342bf8c650fa
devrabaioli <devrabaioli@gmail.com>
2022-12-01 17:53:20 +0000

    twilio SMS

--- backend/src/main/java/dev/rabaioli/dsmeta/controlleres/SaleController.java
+++ backend/src/main/java/dev/rabaioli/dsmeta/controlleres/SaleController.java
@@ -1,12 +1,11 @@
 package dev.rabaioli.dsmeta.controlleres;
 
-import java.util.List;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.rabaioli.dsmeta.entities.Sale;
 import dev.rabaioli.dsmeta.services.SaleService;
+import dev.rabaioli.dsmeta.services.SmsService;
+
 
 @RestController
 @RequestMapping (value = "/sales")
@@ -22,6 +23,8 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public Page<Sale> findSales(
@@ -31,5 +34,11 @@ public class SaleController {
 		
 		return service.findSale(minDate,maxDate,page);
 	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
+    
 
 }
